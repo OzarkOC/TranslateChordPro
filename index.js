528199ed3bc070235f154a069e067988f446d1bd
OzarkOC <ozarkoutpostcenter@gmail.com>
2024-02-15 16:58:16 -0600

    v0.0.8

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 `use strict`;
-import { parseChordPro } from "./utils/chordProParse.ts";
+// import { parseChordPro } from "./utils/chordProParse.ts";
 // Variables
-let songData;
-const display = document.querySelector(`[chordPro="display"]`);
-const transposeUp = document.querySelector(`chordPro="transposeUp`);
-const transposeDown = document.querySelector(`chordPro="transposeDown`);
-const transpose = $(".transpose .transpose-level").data("transpose");
 
+const display = document.querySelector(`[chordPro="display"]`);
+const songData = display.getAttribute("songData");
+const transposeUp = document.querySelector(`[chordPro="transposeUp"]`);
+const transposeDown = document.querySelector(`[chordPro="transposeDown"]`);
+
+// const transpose = $(".transpose .transpose-level").data("transpose");
+console.log(songData);
 // START
-updateDisplay(songData, display, transpose);
+// updateDisplay(songData, display, transpose);
 
 // Functions
 function updateDisplay(chordProData, display, transpose = 1) {
